7a54876c87a908a3fce51ad2f0d86bfa4d1d4069
KevinJonathan117 <kevinjonathan3010@gmail.com>
2021-04-30 18:37:25 +0700

    collection views work, but the layout is messed up, will fix this soon

--- DAcademy/Controller/ViewController/LearnersController.swift
+++ DAcademy/Controller/ViewController/LearnersController.swift
@@ -9,24 +9,44 @@ import UIKit
 
 class LearnersController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
+    @IBOutlet weak var techCollectionView: UICollectionView!
+    @IBOutlet weak var designCollectionView: UICollectionView!
+    @IBOutlet weak var domainExpertCollectionView: UICollectionView!
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if collectionView == designCollectionView {
+            return 20
+        }
+        if collectionView == domainExpertCollectionView {
+            return 10
+        }
         return 70
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let techCell = collectionView.dequeueReusableCell(withReuseIdentifier: "techCell", for: indexPath) as! TechCollectionViewCell
+        let techCell = techCollectionView.dequeueReusableCell(withReuseIdentifier: "techCell", for: indexPath) as! TechCollectionViewCell
         techCell.techText.text = "Tech \(indexPath.item)"
+        
+        if(collectionView == designCollectionView) {
+            let designCell = designCollectionView.dequeueReusableCell(withReuseIdentifier: "designCell", for: indexPath) as! DesignCollectionViewCell
+            designCell.designText.text = "Design \(indexPath.item)"
+            
+            return designCell
+        }
+        if(collectionView == domainExpertCollectionView) {
+            let domainCell = domainExpertCollectionView.dequeueReusableCell(withReuseIdentifier: "domainCell", for: indexPath) as! DomainExpertCollectionViewCell
+            domainCell.domainText.text = "Domain \(indexPath.item)"
+            
+            return domainCell
+        }
         
         return techCell
     }
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        print(indexPath.item)
     }
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         // Do any additional setup after loading the view.
     }
     
